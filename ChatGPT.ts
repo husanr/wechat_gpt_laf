2a2206b65b84d03ec29b49eb28e7704f0afbbff1
husanr <42695679+husanr@users.noreply.github.com>
2023-04-11 12:14:40 +0800

    Update ChatGPT.ts
    优化

--- ChatGPT.ts
+++ ChatGPT.ts
@@ -1,9 +1,43 @@
 // 引入必要的库
 import * as crypto from 'crypto';
 import cloud from '@lafjs/cloud';
+const axios = require('axios');
+
+const OPENAI_KEY = process.env.OPENAI_KEY || 'YOUR API-KEY'; // OpenAI 的 Key
+
+const WAIT_MESSAGE = `处理中 ... \n\n请稍等几秒后发送【1】查看回复`
+const NO_MESSAGE = `暂无内容，请稍后回复【1】再试`
+const CLEAR_MESSAGE = `✅ 记忆已清除`
+const HELP_MESSAGE = `ChatGPT 指令使用指南
+   |    关键字  |   功能         |
+   |      1    | 上一次问题的回复 |
+   |   /clear  |    清除上下文   |
+   |   /help   |   获取更多帮助  |
+  `
+
+const UNSUPPORTED_MESSAGE_TYPES = {
+  image: '暂不支持图片消息',
+  voice: '暂不支持语音消息',
+  video: '暂不支持视频消息',
+  music: '暂不支持音乐消息',
+  news: '暂不支持图文消息',
+}
+
+const OPENAI_MODEL = process.env.MODEL || "gpt-3.5-turbo"; // 使用的 AI 模型
+const OPENAI_MAX_TOKEN = process.env.MAX_TOKEN || 1024; // 最大 token 的值
+
+const LIMIT_HISTORY_MESSAGES = 50 // 限制历史会话最大条数
+const CONVERSATION_MAX_AGE = 60 * 60 * 1000 // 同一会话允许最大周期，默认：1 小时
+const ADJACENT_MESSAGE_MAX_INTERVAL = 10 * 60 * 1000 //同一会话相邻两条消息的最大允许间隔时间，默认：10 分钟
+
+
+// 休眠
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 // 创建数据库连接
 const db = cloud.database();
+
+const Message = db.collection('messages')
 
 // 校验微信服务器发送的消息是否合法
 function verifySignature(signature, timestamp, nonce, token) {
@@ -29,19 +63,135 @@ function toXML(payload, content) {
   `
 }
 
+// 处理文本回复消息
+async function replyText(message) {
+  const { question, sessionId, msgid } = message;
+
+  // 检查是否是重试操作
+  if (question === '1') {
+    const lastMessage = await Message.where({
+      sessionId
+    }).orderBy("createdAt", "desc").get();
+    if (lastMessage.data[0]) {
+      return `${lastMessage.data[0].question}\n------------\n${lastMessage.data[0].answer}`;
+    }
+
+    return NO_MESSAGE;
+  }
+
+  // 发送指令
+  if (question.startsWith('/')) {
+    return await processCommandText(message);
+  }
+
+  // OpenAI 回复内容
+  const prompt = await buildOpenAIPrompt(sessionId, question);
+  const { error, answer } = await getOpenAIReply(prompt);
+  console.debug(`[OpenAI reply] sessionId: ${sessionId}; prompt: ${prompt}; question: ${question}; answer: ${answer}`);
+  if (error) {
+    console.error(`sessionId: ${sessionId}; question: ${question}; error: ${error}`);
+    return error;
+  }
+
+  // 保存消息
+  const token = question.length + answer.length;
+  const result = await Message.add({ token, answer, ...message });
+  console.debug(`[save message] result: ${result}`);
+
+  return answer;
+}
+
+async function processCommandText({ sessionId, question }) {
+  // 清理历史会话
+  if (question === '/clear') {
+    await Message.where({ sessionId }).remove()
+    return CLEAR_MESSAGE;
+  } else {
+    return HELP_MESSAGE;
+  }
+}
+
+
+// 构建 prompt
+async function buildOpenAIPrompt(sessionId, question) {
+  let prompt = [];
+
+  // 获取最近的历史会话
+  const now = Date.now();
+  // const earliestAt = new Date(now.getTime() - CONVERSATION_MAX_AGE)
+  const historyMessages: any = await Message.where({
+    sessionId
+  }).orderBy("createdAt", "desc").limit(LIMIT_HISTORY_MESSAGES).get();
+  // console.log("historyMessages",historyMessages)
+  let lastMessageTime: any = now;
+  let tokenSize = 0;
+  for (const message of historyMessages.data) {
+    // 如果历史会话记录大于 OPENAI_MAX_TOKEN 或 两次会话间隔超过 10 分钟，则停止添加历史会话
+    const timeSinceLastMessage = lastMessageTime ? lastMessageTime - message.createdAt : 0;
+    if (tokenSize > OPENAI_MAX_TOKEN || timeSinceLastMessage > ADJACENT_MESSAGE_MAX_INTERVAL) {
+      await Message.where({}).remove()
+      break
+    }
+
+    prompt.unshift({ role: 'assistant', content: message.answer, });
+    prompt.unshift({ role: 'user', content: message.question, });
+    tokenSize += message.token;
+    lastMessageTime = message.createdAt;
+    // console.log("message", message, lastMessageTime)
+  }
+
+  prompt.push({ role: 'user', content: question });
+  // console.log("Prompt", prompt)
+  return prompt;
+}
+
+// 获取 OpenAI API 的回复
+async function getOpenAIReply(prompt) {
+  const data = JSON.stringify({
+    model: OPENAI_MODEL,
+    messages: prompt
+  });
+
+  const config: any = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api.openai.com/v1/chat/completions',
+    headers: {
+      Authorization: `Bearer ${OPENAI_KEY}`,
+      "Content-Type": "application/json",
+    },
+    data: data,
+    timeout: 50000
+  };
+
+  try {
+    const response: any = await axios(config);
+
+    console.debug(`[OpenAI response] ${response.data}`);
+    if (response.status === 429) {
+      return {
+        error: '问题太多了，我有点眩晕，请稍后再试'
+      }
+    }
+    // 去除多余的换行
+    return {
+      answer: response.data.choices[0].message.content.replace("\n\n", ""),
+    }
+  } catch (e) {
+    console.error(e.response.data);
+    return {
+      error: "问题太难了 出错了. (uДu〃).",
+    }
+  }
+
+}
+
+
 // 处理接收到的消息
 export async function main(event, context) {
   const { signature, timestamp, nonce, echostr } = event.query;
   const token = 'hello123';
-  const { ChatGPTAPI } = await import('chatgpt')
-  // 这里需要把 api 对象放入 cloud.shared 不然无法追踪上下文
-  let api = cloud.shared.get('api')
-  if (!api) {
-    api = new ChatGPTAPI({ apiKey: "YOUR API-KEY" })
-    cloud.shared.set('api', api)
-  }
-  console.log(event)
-  
+
   // 验证消息是否合法，若不合法则返回错误信息
   if (!verifySignature(signature, timestamp, nonce, token)) {
     return 'Invalid signature';
@@ -54,41 +204,38 @@ export async function main(event, context) {
 
   // 处理接收到的消息
   const payload = event.body.xml;
-  console.log("payload",payload)
-  
-  // 查询数据库中是否有上一次的聊天记录
-  const chatData = await db.collection('chat').where({data:{ id: payload.fromusername[0]}}).get();
-  console.log("chatData",chatData)
-  const lastMessage = chatData.data[chatData.data.length - 1];
+  // console.log("payload",payload)
+  // 文本消息
+  if (payload.msgtype[0] === 'text') {
+    const newMessage = {
+      msgid: payload.msgid[0],
+      question: payload.content[0].trim(),
+      username: payload.fromusername[0],
+      sessionId: payload.fromusername[0],
+      createdAt: Date.now()
+    }
 
-  // 如果用户发送的是“1”，则返回上一次的聊天记录
-  if (payload.content[0] === '1') {
-    if(lastMessage) {
-      await db.collection('chat').remove();
-      return toXML(payload, lastMessage.data.message)
-    } else {
-      return toXML(payload, "输入太快咯，请稍后再输入‘1’来获取结果哦!")
+    // 修复请求响应超时问题：如果 5 秒内 AI 没有回复，则返回等待消息
+    const responseText = await Promise.race([
+      replyText(newMessage),
+      sleep(4000.0).then(() => WAIT_MESSAGE),
+    ]);
+    return toXML(payload, responseText);
+  }
+
+  // 事件
+  if (payload.msgtype[0] === 'event') {
+    // 公众号订阅
+    if (payload.event[0] === 'subscribe') {
+      return toXML(payload, HELP_MESSAGE);
     }
   }
 
-  // 调用 ChatGPT API 进行聊天
-  const startTime = Date.now();
-  // console.log(startTime, "---", timestamp+"000")
-  // 如果响应时间大于等于 5s，则返回提示信息给用户
-  if(startTime - (timestamp+"000") > 5000) {
-    const errMessage = "返回内容过长，请稍后回复‘1’以获取最新结果!"
-    return toXML(payload, errMessage)
-  }
-  const response = await api.sendMessage(payload.content[0]);
-  console.log("response",response)
-  const endTime = Date.now();
-
-  // 如果响应时间小于 5s，则直接返回结果给用户
-  if (endTime - startTime < 5000) {
-    return toXML(payload, response.text)
+  // 暂不支持的消息类型
+  if (payload.MsgType in UNSUPPORTED_MESSAGE_TYPES) {
+    const responseText = UNSUPPORTED_MESSAGE_TYPES[payload.MsgType];
+    return toXML(payload, responseText);
   }
 
-  // 如果响应时间大于等于 5s，将结果存入数据库
-  await db.collection('chat').add({ data: { message: response.text, id: payload.fromusername[0] } });
-
+  return 'success'
 }
